0e4f68af5376e1d92056be745ed3eca6617e0b37
forthes <94062997+hiforthes@users.noreply.github.com>
2022-03-31 22:29:20 -0700

    updated xml version code
    <?xml thing was giving error on some servers thats why we got in php tag

--- sitemap.php
+++ sitemap.php
@@ -1,7 +1,7 @@
 <?php header('Content-type: application/xml; charset="utf-8"',true);
 include 'connect.php';
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>' ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php
 $get = $db->prepare("SELECT * from posts");
